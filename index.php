824f23314a1b41d09b46e11449f9cebc9b40c5a4
Philippe Favre <phf.webcreation@gmail.com>
2024-09-27 08:28:37 -0700

    remove index

--- index.php
+++ index.php
@@ -1,59 +1,10 @@
 <html>
   <head>
-    <title>B&amp;B Panorama Lauenen</title>
+    <title>&nbsp;</title>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-    <link href="css/general.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media
-    queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file://
-    -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <link rel="apple-touch-icon" sizes="57x57" href="img/icon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="img/icon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="img/icon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="img/icon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="img/icon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="img/icon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="img/icon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="img/icon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="img/icon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="img/icon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/icon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="img/icon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="img/icon/favicon-16x16.png">
-    <link rel="manifest" href="img/icon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="img/icon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
 
   </head>
   <body>
-    <div style="font-size:20px; padding-top:75px; padding-left:55px; padding-right:55px;">
-      <div style="font-size:25px;text-align:center;margin-bottom:55px;">
-        Welcome to the Panorama Lauenen Bed & Breakfast!
-        <br>
-      </div>
-      <div style="margin-bottom:55px;">
-        Our website is currently under construction, we are sorry for the inconvenience.
-      </div>
-      <div style="margin-bottom:55px;">
-        If you would like to book a room you may follow one of the options below:
-        <ul>
-          <li>Book through <a target="_blank" href="https://www.tripadvisor.com/Hotel_Review-g1157721-d3806523-Reviews-Pension_Panorama_B_B-Lauenen_Bernese_Oberland_Canton_of_Bern.html">Tripadvisor.com</a></li>
-          <li>Find us on <a target="_blank" href="https://goo.gl/maps/iLBDQ6hqFH92">Google</a></li>
-          <li>Contact us directly at <i>info@panorama-lauenen.ch</i> / +41-33-765-31-69 or +41-79-347-90-43. We would be glad to assist you!</li>
-        </ul>
-      </div>
-      <div>
-        We are looking forward to welcoming you!
-      </div>
-    </div>
     
   </body>
 </html>
